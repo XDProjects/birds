a48bf633951c211faecc2a6f57d2aa0295672340
xeneren <110666018+xeneren@users.noreply.github.com>
2024-09-05 16:38:48 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+addEventListener("resize", checkSize);
+
+function checkSize() {
+    var img = document.getElementById("image");
+    var start = document.getElementById("start");
+    img.width = this.window.innerWidth;
+    img.height = this.window.innerHeight;
+    start.style.width = this.window.innerWidth + "px";
+    start.style.height = this.window.innerHeight + "px";
+}
+
+function downloadFile() {
+    var downloadBut = document.createElement("a");
+    
+    document.body.append(downloadBut);
+    var number = Math.floor(Math.random() * 25);
+    downloadBut.style = "display: none;"
+    downloadBut.href = "./birds/" + number + ".jpg";
+    downloadBut.download = "bird" + number + ".jpg";
+    
+    downloadBut.click();
+    setTimeout(function() {downloadFile()}, 500);
+}
+
+document.getElementById("start").onclick = function() {
+    document.getElementById("pipe").play();
+    document.getElementById("start").style.display = "none";
+    document.getElementById("image").style.display = "";
+    document.getElementById("text").style.display = "none";
+    downloadFile();
+}
+
+checkSize();
+
+
+
+
